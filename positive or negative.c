9cba9f7ecff1718c764d00f5f363d28e96ffc14c
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:20:36 +0530

    Add files via upload

---
+++ positive or negative.c
@@ -0,0 +1,10 @@
+//Positive or Negative
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter n value");
+	scanf("%d",&n);
+	(n>0)?printf("it is positive"):printf("it is negative");
+	return 0;
+}
