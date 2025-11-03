8f968012ef5b2039c5f27bf65e4d1fe15e29b719
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:23:38 +0530

    Add files via upload

---
+++ simple interest.c
@@ -0,0 +1,14 @@
+//simple interest
+#include<stdio.h>
+int main()
+{
+	float p,r,t,SI;
+	printf("enter p,r,t values");
+	scanf("%f%f%f",&p,&r,&t);
+	SI=p*r*t/100;
+    printf("SI=%f",SI);
+    return 0;
+}
+    
+	
+
