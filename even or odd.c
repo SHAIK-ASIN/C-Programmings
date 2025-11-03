f6ed7b979a90ca4460212849b6164868717afb3d
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:15:09 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,16 @@
+//even or odd using switch
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter x value");
+	scanf("%d",&x);
+	switch(x%2)
+	{
+		case1:printf("it is even");
+		break;
+		case2:printf("it is odd");
+		break;
+	}
+	return 0;
+}
