cf445815f79db6a7a4229d24ae067c060b09b233
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:28:35 +0530

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,15 @@
+//1 to 10 natural numbers
+#include<stdio.h>
+int main()
+{
+	int i=1,n;
+	printf("enter n value");
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		printf("%d",i);
+		i++;
+	}
+	return 0;
+}
+
