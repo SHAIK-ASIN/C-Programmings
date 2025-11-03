d0f831a1d6f8fbb5c413f0b7faa610d1d1b420c9
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:06:35 +0530

    Add files via upload

---
+++ 1 to N natural numbers.c
@@ -0,0 +1,13 @@
+//1 to N natural numbers using for loop
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter n value");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d",i);
+	}
+	return 0;
+}
