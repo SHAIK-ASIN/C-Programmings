29079dcd8efac24a3a0b1ffda59cd7d904aa9ff2
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:28:09 +0530

    Add files via upload

---
+++ unary operators.c
@@ -0,0 +1,17 @@
+//unary operators
+#include<stdio.h>
+int main()
+{
+	int x=10,p,q,r,s;
+	p=++x;
+	printf("\n p=%d",p);
+	q=x--;
+	printf("\n q=%d",q);
+	r=x++;
+	printf("\n r=%d",r);
+	s=--x;
+	printf("\n s=%d",s);
+	printf("\n x=%d",x);
+	return 0;
+}
+
