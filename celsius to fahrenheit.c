4bdf666fb4fea7a6102f1f1ef140f0adbed43346
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:12:43 +0530

    Add files via upload

---
+++ celsius to fahrenheit.c
@@ -0,0 +1,13 @@
+//Celsius to Fahrenheit
+#include<stdio.h>
+int main()
+{
+	float c,f;
+	printf("enter celsius value");
+	scanf("%f",&c);
+	f=(1.8*c)+32;
+	printf("fahrenheit value is=%f",f);
+	return 0;
+}
+
+
