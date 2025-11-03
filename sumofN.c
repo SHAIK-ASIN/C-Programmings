8ee39519156b5d07919cbba00eabe138d2687f34
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:27:17 +0530

    Add files via upload

---
+++ sumofN.c
@@ -0,0 +1,14 @@
+//sum of natural numbers (for loop)
+#include<stdio.h>
+int main()
+{
+	int i,n,sum=0;
+	printf("enter n value");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+i;
+	}
+	printf("sum is=%d",sum);
+	return 0;
+}
