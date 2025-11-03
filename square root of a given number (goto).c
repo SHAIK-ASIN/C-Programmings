0c8d130b81245d4e6473c07749fa63be5fd6d02a
SHAIK-ASIN <asinshaik046@gmail.com>
2025-11-03 19:24:03 +0530

    Add files via upload

---
+++ square root of a given number (goto).c
@@ -0,0 +1,15 @@
+//square root of given number
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,s;
+	hi:
+	printf("enter a number");
+	scanf("%d",&n);
+	if(n<0)
+	goto hi;
+	s=sqrt(n);
+	printf("square root=%d",s);
+	return 0;
+}
